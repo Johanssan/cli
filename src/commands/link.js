3e2da3a951b853d472a472652b746f3da743cde5
Ivo Katunaric <ikatun@Ivos-MacBook-Pro.local>
2017-08-28 13:06:11 +0200

    Update linker

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,12 +1,10 @@
 import path from 'path';
-import { ensureInExtensionDir, readJsonFile } from '../extension/data';
+import { ensureInExtensionDir, readJsonFile, loadExtensionJsonAsync } from '../extension/data';
 import { getPlatformRootDir } from '../extension/platform';
 
 export default async function link(args) {
   const extensionRoot = ensureInExtensionDir();
   const clonedAppPath = await getPlatformRootDir(args.clonedAppPath);
 
-  await readJsonFile(path.join(clonedAppPath, 'package.json'))
+  await readJsonFile(path.join(clonedAppPath, 'package.json'));
 }
-
-
